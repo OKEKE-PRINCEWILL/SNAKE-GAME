96a990c71bf68bae588ff9290f1ca9b0ef845de6
OKEKE-PRINCEWILL <okesprince2005@gmail.com>
2025-01-02 11:47:08 +0100

    first commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+}
+
